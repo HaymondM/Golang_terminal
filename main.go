efc7bbb3d534f926408802944a1f11ec3b73f75d
Marshall Haymond <68209576+HaymondM@users.noreply.github.com>
2021-03-04 07:06:24 -0700

    added a switch statement

--- main.go
+++ main.go
@@ -15,18 +15,32 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println("Welcome back " + user.Name)
+	fmt.Println("Welcome " + user.Name)
 
-	fmt.Printf("If you need help do --options to see your options \n")
-	var input string
-	fmt.Scan("<<", input)
-	fmt.Printf("%s", input)
-
-	var x = (rand.Intn(39)) //1 way to declare a var
-	y := "plus"             //2nd way to declare a var
-
-	rand.Seed(time.Now().UnixNano())           //to make a new rand num every time. Default seed is 1
-	fmt.Println("This is a random number:", x) //adds a space by itself
-	fmt.Println(9, y, 9, "=", 9+9)
-
+	fmt.Print("If you need help do --options to see your options \n")
+	for {
+		fmt.Print("<< ")
+		var input string
+		fmt.Scanln(&input)
+		switch input {
+		case "--options":
+			fmt.Print("Here are the options")
+		case "rand":
+			var x = (rand.Intn(77))          //1 way to declare a var
+			rand.Seed(time.Now().UnixNano()) //to make a new rand num every time. Default seed is 1
+			fmt.Println("This is a random number:", x)
+		case "quit":
+			fmt.Print("Bye")
+			break
+		default:
+			fmt.Println("That is not a option")
+		}
+		//if strings.Compare(input, "options") == 1 {
+		//	fmt.Print("Here are the options")
+		//	break
+		//}
+		if input == "quit" {
+			break
+		}
+	}
 }
